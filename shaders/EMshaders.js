08ba05313e8c0dec917be4da02ebc16f14dd4ef0
Emmanuel Bustos <ema2159@gmail.com>
2021-03-26 13:56:01 +0100

    Add: Visualization for different color coordinates in elevation map

--- shaders/EMshaders.js
+++ shaders/EMshaders.js
@@ -2,20 +2,88 @@ const EMvertexShader = `
 varying vec2 vUv;
 uniform float scaleElevation; 
 uniform int chanel;
+uniform int coordSystem;
 uniform sampler2D image;
+
+#define PI 3.14159265358979323846
+
+vec3 rgb2xyz(vec3 c) {
+  vec3 tmp;
+  tmp.x = (c.r > 0.04045) ? pow((c.r + 0.055) / 1.055, 2.4) : c.r / 12.92;
+  tmp.y = (c.g > 0.04045) ? pow((c.g + 0.055) / 1.055, 2.4) : c.g / 12.92;
+  tmp.z = (c.b > 0.04045) ? pow((c.b + 0.055) / 1.055, 2.4) : c.b / 12.92;
+  mat3 xyz_mat = mat3(0.4124, 0.3576, 0.1805, 0.2126, 0.7152, 0.0722, 0.0193,
+                      0.1192, 0.9505);
+  return xyz_mat * tmp;
+}
+
+vec3 rgb2lab(vec3 c) {
+  vec3 xyz_norm = 100.0 * rgb2xyz(c) / vec3(95.047, 100.0, 108.883);
+
+  vec3 lab;
+  float delta = 6.0 / 29.0;
+
+  lab.x = (xyz_norm.x > pow(delta, 3.0))
+               ? pow(xyz_norm.x, 1.0 / 3.0)
+               : (((1.0 / 3.0 * pow(delta, 2.0)) * xyz_norm.x) + (4.0 / 29.0));
+  lab.y = (xyz_norm.y > pow(delta, 3.0))
+               ? pow(xyz_norm.y, 1.0 / 3.0)
+               : (((1.0 / 3.0 * pow(delta, 2.0)) * xyz_norm.y) + (4.0 / 29.0));
+  lab.z = (xyz_norm.z > pow(delta, 3.0))
+               ? pow(xyz_norm.z, 1.0 / 3.0)
+               : (((1.0 / 3.0 * pow(delta, 2.0)) * xyz_norm.z) + (4.0 / 29.0));
+
+  vec3 tmp;
+  tmp.x = (116.0 * lab.y) - 16.0;
+  tmp.y = 500.0 * (lab.x - lab.y);
+  tmp.z = 200.0 * (lab.y - lab.z);
+
+  return tmp;
+}
+
+vec3 rgb2hsv(vec3 c) {
+  vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+  vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+  vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+  float d = q.x - min(q.w, q.y);
+  float e = 1.0e-10;
+  return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
 
 void main() {
   vUv = uv;
   vec3 color = texture2D ( image, vUv ).rgb;
   float l;
+  vec3 height;
+  // Coord system transformation
+  // RGB
+  if (coordSystem == 0) {
+    height = color;
+  }
+  // XYZ
+  else if (coordSystem == 1) {
+    height = rgb2xyz(color);
+  }
+  // Lab
+  else if (coordSystem == 2) {
+    vec3 lab_col = rgb2lab(color);
+    height.x = lab_col[0]/100.0;
+    height.y = (lab_col[2]+86.185) / 184.439;
+    height.z = (lab_col[1]+107.863) / 202.345;
+  }
+  // HSV
+  else if (coordSystem == 3) {
+    height = rgb2hsv(color);
+  }
   if(chanel==0) {
-    l = length ( color );
+    l = length ( height );
   } else if(chanel==1) {
-    l = color.r;
+    l = height.r;
   } else if(chanel==2) {
-    l = color.g;
+    l = height.g;
   } else if(chanel==3) {
-    l = color.b;
+    l = height.b;
   }
   vec3 tmp = position;
   tmp.z = tmp.z + l*scaleElevation;
@@ -25,17 +93,20 @@ void main() {
 const EMfragmentShader = `
 varying vec2 vUv;
 uniform sampler2D tex;
+uniform int coordSystem;
 uniform int chanel;
 
 void main() {
   vec3 color = texture2D ( tex, vUv ).rgb;
-  if(chanel==0) {
-  } else if(chanel==1) {
-    color = vec3(color.r, 0.0, 0.0);
-  } else if(chanel==2) {
-    color = vec3(0.0, color.g, 0.0);
-  } else if(chanel==3) {
-    color = vec3(0.0, 0.0, color.b);
+  if(coordSystem==0) {
+    if(chanel==0) {
+    } else if(chanel==1) {
+      color = vec3(color.r, 0.0, 0.0);
+    } else if(chanel==2) {
+      color = vec3(0.0, color.g, 0.0);
+    } else if(chanel==3) {
+      color = vec3(0.0, 0.0, color.b);
+    }
   }
   gl_FragColor.rgb = color;
   gl_FragColor.a = 1.0;
--- shaders/ElevationMap/fragmentShader.frag
+++ shaders/ElevationMap/fragmentShader.frag
@@ -1,16 +1,19 @@
 varying vec2 vUv;
 uniform sampler2D tex;
+uniform int coordSystem;
 uniform int chanel;
 
 void main() {
   vec3 color = texture2D ( tex, vUv ).rgb;
-  if(chanel==0) {
-  } else if(chanel==1) {
-    color = vec3(color.r, 0.0, 0.0);
-  } else if(chanel==2) {
-    color = vec3(0.0, color.g, 0.0);
-  } else if(chanel==3) {
-    color = vec3(0.0, 0.0, color.b);
+  if(coordSystem==0) {
+    if(chanel==0) {
+    } else if(chanel==1) {
+      color = vec3(color.r, 0.0, 0.0);
+    } else if(chanel==2) {
+      color = vec3(0.0, color.g, 0.0);
+    } else if(chanel==3) {
+      color = vec3(0.0, 0.0, color.b);
+    }
   }
   gl_FragColor.rgb = color;
   gl_FragColor.a = 1.0;
