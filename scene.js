1c5ae56854b602706d7b9722f8062c26a64b278b
Emmanuel Bustos <ema2159@gmail.com>
2021-04-20 19:01:39 +0200

    Mod: Pin three.js version

--- scene.js
+++ scene.js
@@ -1,6 +1,6 @@
-import * as THREE from "https://unpkg.com/three/build/three.module.js";
-import { VRButton } from 'https://unpkg.com/three/examples/jsm/webxr/VRButton.js';
-import { OrbitControls } from "https://unpkg.com/three/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import { VRButton } from 'https://unpkg.com/three@0.126.1/examples/jsm/webxr/VRButton.js';
+import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js";
 import { Interaction } from "./vendor/three\.interaction/build/three\.interaction\.module.js";
 import "./vendor/uil/build/uil.js";
 import {EMvertexShader, EMfragmentShader} from "./shaders/EMshaders.js";
